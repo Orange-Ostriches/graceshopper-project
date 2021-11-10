2d8332cac9e1377b7fbf1290ec7d4892da683103
Zack Ward <wardzb7@gmail.com>
2021-11-09 23:58:29 -0500

    refactored all products to a functional component via hooks

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { connect, useSelector, useDispatch } from 'react-redux';
 import { fetchProducts } from '../store/products';
 
-class AllProducts extends React.Component {
+const AllProducts = () => {
+
+  const { products } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const run = async () => {
+      dispatch(fetchProducts());
+    };
+    run();
+  }, [])
+
+  return (
+    <div id="all-products">
+        <div id="products">
+          {products.map(product => (
+            <div key={product.id} id="product">
+              <img width="200px" src={product.image} />
+              <h1>{product.name}</h1>
+              <h3>{product.price}</h3>
+              <h4>{product.fuelType}</h4>
+              <h4>{product.size}</h4>
+              <h4>{product.range}</h4>
+              <h4>{product.specialty}</h4>
+              <h4>{product.topSpeed} lyph</h4>
+              <h4>{product.autopilot}</h4>
+              <p>{product.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+  )
+}
+
+export default AllProducts;
+
+/**
+class AllProducts extends Component {
 
   componentDidMount() {
     this.props.getProducts()
@@ -46,3 +83,6 @@ const mDTP = dispatch => {
 };
 
 export default connect(mSTP, mDTP)(AllProducts);
+
+*/
+
