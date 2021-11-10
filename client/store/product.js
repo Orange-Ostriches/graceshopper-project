6a7476b2a604ce7885c17350f855ddd9b38667b4
seungdonchoi <dchoifor2@gmail.com>
2021-11-09 23:45:18 -0800

    added Single Product Component

---
+++ client/store/product.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const SINGLE_PRODUCT = "SINGLE_PRODUCT"
+
+const getSingleProduct = product => ({
+  type: SINGLE_PRODUCT,
+  product
+});
+
+export const fetchSingleProduct = (id) => async dispatch => {
+  try {
+    const { data } = await axios.get(`/api/spaceships/${id}`)
+    dispatch(getSingleProduct(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default function (state = {}, action) {
+  switch (action.type) {
+    case SINGLE_PRODUCT:
+      return action.product
+    default:
+      return state
+  }
+}
