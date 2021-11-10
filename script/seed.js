1186dd56d4c32d7e493ce80bdb778faff4a1d6ec
Rusty Porter <rusty.porter@gmail.com>
2021-11-10 11:29:45 -0700

    Completed Cart model, associations, and seeding
    Also added an API route to see all the carts in the model. Not super useful for production, but was useful to test with. This could be edited later to get whatever cart has the userId of the logged in user probably.

--- script/seed.js
+++ script/seed.js
@@ -1,59 +1,133 @@
-'use strict'
+"use strict";
 
-const {db, models: {Spaceship, User} } = require('../server/db')
+const {
+  db,
+  models: { Spaceship, User, Cart },
+} = require("../server/db");
 
-const spaceships = [{
-  name: 'WarpTrace 9000',
-  price: 175,
-  fuelType: 'uranium',
-  size: 'medium',
-  range: 1500,
-  specialty: 'transportation',
-  topSpeed: 300,
-  autopilot: true,
-  description: 'The newest addition to the WarpTrace fleet, this spectacular spacehip will get you where you need to go faster than you can say "SOLD!".',
-},{
-  name: 'GigaBlaster Mk7',
-  price: 400,
-  fuelType: 'hydrogen',
-  size: 'large',
-  range: 1000,
-  specialty: 'combat',
-  topSpeed: 420,
-  autopilot: false,
-  description: "If you have enemies that need blastin', look no further.",
-  image: 'https://64.media.tumblr.com/0a798505b52e0589d0760e56bec8219e/tumblr_odtma4Id6G1vu9wkoo1_1280.jpg'
-},{
-  name: 'Driller',
-  price: 250,
-  fuelType: 'plutonium',
-  size: 'large',
-  range: 2500,
-  specialty: 'transportation',
-  topSpeed: 100,
-  autopilot: true,
-  description: "The spaceship naming budget dried up after all R&D was funneled into more important features... See for yourself.",
-  image: 'https://cdnb.artstation.com/p/assets/images/images/007/980/509/large/nirmal-thomas-banner60.jpg?1620629708'
-}];
+const spaceships = [
+  {
+    name: "WarpTrace 9000",
+    price: 175,
+    fuelType: "uranium",
+    size: "medium",
+    range: 1500,
+    specialty: "transportation",
+    topSpeed: 300,
+    autopilot: true,
+    description:
+      'The newest addition to the WarpTrace fleet, this spectacular spacehip will get you where you need to go faster than you can say "SOLD!".',
+  },
+  {
+    name: "GigaBlaster Mk7",
+    price: 400,
+    fuelType: "hydrogen",
+    size: "large",
+    range: 1000,
+    specialty: "combat",
+    topSpeed: 420,
+    autopilot: false,
+    description: "If you have enemies that need blastin', look no further.",
+    image:
+      "https://64.media.tumblr.com/0a798505b52e0589d0760e56bec8219e/tumblr_odtma4Id6G1vu9wkoo1_1280.jpg",
+  },
+  {
+    name: "Driller",
+    price: 250,
+    fuelType: "plutonium",
+    size: "large",
+    range: 2500,
+    specialty: "transportation",
+    topSpeed: 100,
+    autopilot: true,
+    description:
+      "The spaceship naming budget dried up after all R&D was funneled into more important features... See for yourself.",
+    image:
+      "https://cdnb.artstation.com/p/assets/images/images/007/980/509/large/nirmal-thomas-banner60.jpg?1620629708",
+  },
+];
 
-const users = [{
-  username: 'cody',
-  password: '123'
-}, {
-  username: 'murphy',
-  password: '123'
-}, {
-  username: 'goodguygary',
-  password: 'notasnail123'
-}];
+const users = [
+  {
+    username: "cody",
+    password: "123",
+  },
+  {
+    username: "murphy",
+    password: "123",
+  },
+  {
+    username: "goodguygary",
+    password: "notasnail123",
+  },
+];
+
+const carts = [
+  {
+    products: [
+      {
+        name: "Driller",
+        price: 250,
+        fuelType: "plutonium",
+        size: "large",
+        range: 2500,
+        specialty: "transportation",
+        topSpeed: 100,
+        autopilot: true,
+        description:
+          "The spaceship naming budget dried up after all R&D was funneled into more important features... See for yourself.",
+        image:
+          "https://cdnb.artstation.com/p/assets/images/images/007/980/509/large/nirmal-thomas-banner60.jpg?1620629708",
+      },
+      {
+        name: "GigaBlaster Mk7",
+        price: 400,
+        fuelType: "hydrogen",
+        size: "large",
+        range: 1000,
+        specialty: "combat",
+        topSpeed: 420,
+        autopilot: false,
+        description: "If you have enemies that need blastin', look no further.",
+        image:
+          "https://64.media.tumblr.com/0a798505b52e0589d0760e56bec8219e/tumblr_odtma4Id6G1vu9wkoo1_1280.jpg",
+      },
+    ],
+    subtotal: 650,
+    userId: 2,
+  },
+  {
+    products: null,
+    subtotal: 0,
+    userId: 3,
+  },
+  {
+    products: [
+      {
+        name: "WarpTrace 9000",
+        price: 175,
+        fuelType: "uranium",
+        size: "medium",
+        range: 1500,
+        specialty: "transportation",
+        topSpeed: 300,
+        autopilot: true,
+        description:
+          'The newest addition to the WarpTrace fleet, this spectacular spacehip will get you where you need to go faster than you can say "SOLD!".',
+      },
+    ],
+    subtotal: 175,
+    userId: 1,
+  },
+];
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   // Creating Users
   // const users = await Promise.all([
@@ -61,16 +135,28 @@ async function seed() {
   //   User.create({ username: 'murphy', password: '123' }),
   // ])
 
-  await Promise.all(spaceships.map(spaceship => {
-    return Spaceship.create(spaceship)
-    }))
+  await Promise.all(
+    spaceships.map((spaceship) => {
+      return Spaceship.create(spaceship);
+    })
+  );
 
-  await Promise.all(users.map(user => {
-    return User.create(user)
-  }))
+  await Promise.all(
+    users.map((user) => {
+      return User.create(user);
+    })
+  );
 
-  console.log(`seeded ${spaceships.length} spaceships and ${users.length} users`)
-  console.log(`seeded successfully!`)
+  await Promise.all(
+    carts.map((cart) => {
+      return Cart.create(cart);
+    })
+  );
+
+  console.log(
+    `seeded ${spaceships.length} spaceships, ${users.length} users, and ${carts.length} carts`
+  );
+  console.log(`seeded successfully!`);
   // return {
   //   users: {
   //     cody: users[0],
@@ -85,16 +171,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -104,8 +190,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
