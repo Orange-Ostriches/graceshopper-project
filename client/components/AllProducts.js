a5f1ad9679f672be8c9fc19a1bf1d17d37d7d966
Rusty Porter <rusty.porter@gmail.com>
2021-11-16 22:01:17 -0700

    Navbar appears when scrolling up, disappears when scrolling down
    Kind of a weird animation, and not perfect, but kinda cool.

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,16 +1,15 @@
-import React, { Component, useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchProducts } from '../store/products';
-import { Link } from 'react-router-dom';
+import React, { Component, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts } from "../store/products";
+import { Link } from "react-router-dom";
 
 export const AllProducts = () => {
-
-  const { products } = useSelector(state => state);
+  const { products } = useSelector((state) => state);
   const [priceFilter, setPriceFilter] = useState(0);
-  const [fuelTypeFilter, setFuelTypeFilter] = useState('all');
-  const [sizeFilter, setSizeFilter] = useState('all');
+  const [fuelTypeFilter, setFuelTypeFilter] = useState("all");
+  const [sizeFilter, setSizeFilter] = useState("all");
   const [rangeFilter, setRangeFilter] = useState(0);
-  const [specialtyFilter, setSpecialtyFilter] = useState('all');
+  const [specialtyFilter, setSpecialtyFilter] = useState("all");
   const [topSpeedFilter, setTopSpeedFilter] = useState(0);
   const [autopilotFilter, setAutopilotFilter] = useState(2);
   const dispatch = useDispatch();
@@ -21,16 +20,16 @@ export const AllProducts = () => {
 
   const clearFilter = () => {
     setPriceFilter(0);
-    setFuelTypeFilter('all');
-    setSizeFilter('all');
+    setFuelTypeFilter("all");
+    setSizeFilter("all");
     setRangeFilter(0);
-    setSpecialtyFilter('all');
+    setSpecialtyFilter("all");
     setTopSpeedFilter(0);
     setAutopilotFilter(2);
   };
 
   return (
-    <div>
+    <div className="content">
       <div id="filters">
         <h4>Filter by:</h4>
         <form>
@@ -39,7 +38,7 @@ export const AllProducts = () => {
             <select
               name="price"
               value={priceFilter}
-              onChange={e => setPriceFilter(parseInt(e.target.value))}
+              onChange={(e) => setPriceFilter(parseInt(e.target.value))}
             >
               <option value={0}>All</option>
               <option value={499}>100 - 499</option>
@@ -51,7 +50,7 @@ export const AllProducts = () => {
             <select
               name="fuelType"
               value={fuelTypeFilter}
-              onChange={e => setFuelTypeFilter(e.target.value)}
+              onChange={(e) => setFuelTypeFilter(e.target.value)}
             >
               <option value="all">All</option>
               <option value="Plutonium">Plutonium</option>
@@ -64,7 +63,7 @@ export const AllProducts = () => {
             <select
               name="size"
               value={sizeFilter}
-              onChange={e => setSizeFilter(e.target.value)}
+              onChange={(e) => setSizeFilter(e.target.value)}
             >
               <option value="all">All</option>
               <option value="Small">Small</option>
@@ -77,7 +76,7 @@ export const AllProducts = () => {
             <select
               name="range"
               value={rangeFilter}
-              onChange={e => setRangeFilter(parseInt(e.target.value))}
+              onChange={(e) => setRangeFilter(parseInt(e.target.value))}
             >
               <option value={0}>All</option>
               <option value={499}>100 - 499</option>
@@ -92,7 +91,7 @@ export const AllProducts = () => {
             <select
               name="specialty"
               value={specialtyFilter}
-              onChange={e => setSpecialtyFilter(e.target.value)}
+              onChange={(e) => setSpecialtyFilter(e.target.value)}
             >
               <option value="all">All</option>
               <option value="Combat">Combat</option>
@@ -106,7 +105,7 @@ export const AllProducts = () => {
             <select
               name="topSpeed"
               value={topSpeedFilter}
-              onChange={e => setTopSpeedFilter(parseInt(e.target.value))}
+              onChange={(e) => setTopSpeedFilter(parseInt(e.target.value))}
             >
               <option value={0}>All</option>
               <option value={249}>100 - 249</option>
@@ -118,86 +117,99 @@ export const AllProducts = () => {
             <select
               name="autopilot"
               value={autopilotFilter}
-              onChange={e => setAutopilotFilter(parseInt(e.target.value))}
+              onChange={(e) => setAutopilotFilter(parseInt(e.target.value))}
             >
               <option value={2}>All</option>
               <option value={1}>Yes</option>
               <option value={0}>No</option>
             </select>
           </div>
-          <button
-            value="clear"
-            type="button"
-            onClick={() => clearFilter()}
-          >
+          <button value="clear" type="button" onClick={() => clearFilter()}>
             Clear Filter
           </button>
         </form>
       </div>
-      {
-        products.length < 1 ? (
-          <h1 className="title">Loading...</h1>
-        ) : (
+      {products.length < 1 ? (
+        <h1 className="title">Loading...</h1>
+      ) : (
         <div id="all-products">
           <div id="products">
-            {
-              products
-                .filter(product => {
-                  const priceRanges = {499: 100, 1000: 500}
-                  if (priceFilter === 0) return product
-                  if (product.price <= priceFilter && product.price >= priceRanges[priceFilter]) {
-                    return product
-                  }
-                })
-                .filter(product => {
-                  if (fuelTypeFilter === 'all') return product
-                  return product.fuelType === fuelTypeFilter
-                })
-                .filter(product => {
-                  if (sizeFilter === 'all') return product
-                  return product.size === sizeFilter
-                })
-                .filter(product => {
-                  const rangeRanges = {499: 100, 999: 500, 1499: 1000, 1999: 1500, 2500: 2000}
-                  if (rangeFilter === 0) return product
-                  if (product.range <= rangeFilter && product.range >= rangeRanges[rangeFilter]) {
-                    return product
-                  }
-                })
-                .filter(product => {
-                  if (specialtyFilter === 'all') return product
-                  return product.specialty === specialtyFilter
-                })
-                .filter(product => {
-                  const topSpeedRanges = {249: 100, 500: 250}
-                  if (topSpeedFilter === 0) return product
-                  if (product.topSpeed <= topSpeedFilter && product.topSpeed >= topSpeedRanges[topSpeedFilter]) {
-                    return product
-                  }
-                })
-                .filter(product => {
-                  if (autopilotFilter === 2) return product
-                  if (product.autopilot && autopilotFilter === 1) return product
-                  if (!product.autopilot && autopilotFilter === 0) return product
-                })
-                .map(product => (
-                  <div key={product.id} id="product">
-                    <Link to={`/products/${product.id}`}>
-                      <img className="prod-img" src={product.image} />
-                    </Link>
-                    <h1>{product.name}</h1>
-                    <h3>Price: {product.price}</h3>
-                    <h4>Fuel Type: {product.fuelType}</h4>
-                    <h4>Size: {product.size}</h4>
-                    <h4>Range: {product.range}</h4>
-                    <h4>Specialty: {product.specialty}</h4>
-                    <h4>Top Speed: {product.topSpeed} lyph</h4>
-                    <h4>Autopilot? {product.autopilot ? "Yes" : "No"}</h4>
-                    <p id="desc">Description: {product.description}</p>
-                  </div>
-                )
-              )
-            }
+            {products
+              .filter((product) => {
+                const priceRanges = { 499: 100, 1000: 500 };
+                if (priceFilter === 0) return product;
+                if (
+                  product.price <= priceFilter &&
+                  product.price >= priceRanges[priceFilter]
+                ) {
+                  return product;
+                }
+              })
+              .filter((product) => {
+                if (fuelTypeFilter === "all") return product;
+                return product.fuelType === fuelTypeFilter;
+              })
+              .filter((product) => {
+                if (sizeFilter === "all") return product;
+                return product.size === sizeFilter;
+              })
+              .filter((product) => {
+                const rangeRanges = {
+                  499: 100,
+                  999: 500,
+                  1499: 1000,
+                  1999: 1500,
+                  2500: 2000,
+                };
+                if (rangeFilter === 0) return product;
+                if (
+                  product.range <= rangeFilter &&
+                  product.range >= rangeRanges[rangeFilter]
+                ) {
+                  return product;
+                }
+              })
+              .filter((product) => {
+                if (specialtyFilter === "all") return product;
+                return product.specialty === specialtyFilter;
+              })
+              .filter((product) => {
+                const topSpeedRanges = { 249: 100, 500: 250 };
+                if (topSpeedFilter === 0) return product;
+                if (
+                  product.topSpeed <= topSpeedFilter &&
+                  product.topSpeed >= topSpeedRanges[topSpeedFilter]
+                ) {
+                  return product;
+                }
+              })
+              .filter((product) => {
+                if (autopilotFilter === 2) return product;
+                if (product.autopilot && autopilotFilter === 1) return product;
+                if (!product.autopilot && autopilotFilter === 0) return product;
+              })
+              .map((product) => (
+                <div key={product.id} id="product">
+                  <Link to={`/products/${product.id}`}>
+                    <img className="prod-img" src={product.image} />
+                  </Link>
+                  <h1>{product.name}</h1>
+                  <h3>Price: {product.price}</h3>
+                  <h4>Fuel Type: {product.fuelType}</h4>
+                  <h4>Size: {product.size}</h4>
+                  <h4>Range: {product.range}</h4>
+                  <h4>Specialty: {product.specialty}</h4>
+                  <h4>Top Speed: {product.topSpeed} lyph</h4>
+                  <h4>Autopilot? {product.autopilot ? "Yes" : "No"}</h4>
+                  <p id="desc">Description: {product.description}</p>
+                </div>
+              ))}
+            {products[0] === undefined ? (
+              <h3>
+                Can't find your perfect starship? Contact Orange Ostriches
+                SpaceCo about custom builds!
+              </h3>
+            ) : null}
           </div>
         </div>
       )}
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,75 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-// document.addEventListener("scroll", (event) => {
-//   console.log(`scrollTop: ${window.document.scrollTop}`);
-// });
+const Navbar = ({ handleClick, isLoggedIn, isAdmin }) => {
+  const [scrollY, setScrollY] = useState(0);
+  const [newScroll, setNewScroll] = useState(0);
+  let showNav = true;
 
-const Navbar = ({ handleClick, isLoggedIn, isAdmin }) => (
-  <nav className="navbar">
-    <div>
-      {isLoggedIn ? (
-        <div className="navbar-nav">
-          <div className="nav-left">
-            {/* The navbar will show these links after you log in */}
-            <Link to="/" className="nav-link">
-              <img id="logo" src="/images/logo.png" />
-              <span className="link-text">Home</span>
-            </Link>
-            <Link to="/products" className="nav-link">
-              <FontAwesomeIcon icon={["fad", "rocket-launch"]} />
-              <span className="link-text">Products</span>
-            </Link>
-          </div>
-          <div className="nav-right">
-            {isAdmin ? (
-              <Link to="/admin-portal" className="nav-link">
-                <FontAwesomeIcon icon={["fad", "solar-system"]} />
-                <span className="link-text">Admin Portal</span>
+  if (scrollY < newScroll) {
+    showNav = false;
+  }
+
+  const scrollHandler = () => {
+    setNewScroll(window.scrollY);
+    // else {
+    //     showNav = true;
+    //   }
+  };
+  setTimeout(() => {
+    setScrollY(newScroll);
+  }, 1000);
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, []);
+
+  return (
+    <nav className={!showNav ? "navbar-hidden" : "navbar"}>
+      <div>
+        {isLoggedIn ? (
+          <div className="navbar-nav">
+            <div className="nav-left">
+              {/* The navbar will show these links after you log in */}
+              <Link to="/" className="nav-link">
+                <img id="logo" src="/images/logo.png" />
+                <span className="link-text">Home</span>
               </Link>
-            ) : (
+              <Link to="/products" className="nav-link">
+                <FontAwesomeIcon icon={["fad", "rocket-launch"]} />
+                <span className="link-text">Products</span>
+              </Link>
+            </div>
+            <div className="nav-right">
+              {isAdmin ? (
+                <Link to="/admin-portal" className="nav-link">
+                  <FontAwesomeIcon icon={["fad", "solar-system"]} />
+                  <span className="link-text">Admin Portal</span>
+                </Link>
+              ) : (
+                <Link to="/cart" className="nav-link">
+                  <FontAwesomeIcon icon={["fad", "shopping-cart"]} />
+                  <span className="link-text">Cart</span>
+                </Link>
+              )}
+              <a className="nav-link" href="#" onClick={handleClick}>
+                <FontAwesomeIcon icon={["fad", "sign-out"]} />
+                <span className="link-text">Logout</span>
+              </a>
+            </div>
+          </div>
+        ) : (
+          <div className="navbar-nav">
+            {/* The navbar will show these links before you log in */}
+            <div className="nav-left">
+              <Link to="/" className="nav-link">
+                <img id="logo" src="/images/logo.png" />
+                <span className="link-text">Home</span>
+              </Link>
+              <Link to="/products" className="nav-link">
+                <FontAwesomeIcon icon={["fad", "rocket-launch"]} />
+                <span className="link-text">Products</span>
+              </Link>
+            </div>
+            <div className="nav-right">
               <Link to="/cart" className="nav-link">
                 <FontAwesomeIcon icon={["fad", "shopping-cart"]} />
                 <span className="link-text">Cart</span>
               </Link>
-            )}
-            <a className="nav-link" href="#" onClick={handleClick}>
-              <FontAwesomeIcon icon={["fad", "sign-out"]} />
-              <span className="link-text">Logout</span>
-            </a>
+              <Link to="/login" className="nav-link">
+                <FontAwesomeIcon icon={["fad", "user-astronaut"]} />
+                <span className="link-text">Login</span>
+              </Link>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="navbar-nav">
-          {/* The navbar will show these links before you log in */}
-          <div className="nav-left">
-            <Link to="/" className="nav-link">
-              <img id="logo" src="/images/logo.png" />
-              <span className="link-text">Home</span>
-            </Link>
-            <Link to="/products" className="nav-link">
-              <FontAwesomeIcon icon={["fad", "rocket-launch"]} />
-              <span className="link-text">Products</span>
-            </Link>
-          </div>
-          <div className="nav-right">
-            <Link to="/cart" className="nav-link">
-              <FontAwesomeIcon icon={["fad", "shopping-cart"]} />
-              <span className="link-text">Cart</span>
-            </Link>
-            <Link to="/login" className="nav-link">
-              <FontAwesomeIcon icon={["fad", "user-astronaut"]} />
-              <span className="link-text">Login</span>
-            </Link>
-          </div>
-        </div>
-      )}
-    </div>
-  </nav>
-);
+        )}
+      </div>
+    </nav>
+  );
+};
 
 /**
  * CONTAINER
