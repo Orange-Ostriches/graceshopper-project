1dd74fd8ee1d9eb22ff4bbe96dfc4ff724d8f3c8
Rusty Porter <rusty.porter@gmail.com>
2022-04-22 22:09:47 -0700

    Started nav bar - transition on hover

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,43 +4,38 @@ import { Link } from "react-router-dom";
 import { logout } from "../store";
 
 const Navbar = ({ handleClick, isLoggedIn, isAdmin }) => (
-  <div>
-    <div id='logo-name'>
-      <img id='logo' src='/images/logo.png' />
-      <h1>Orange Ostriches SpaceCo</h1>
-    </div>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/">Home</Link>
-          <Link to="/products">All Products</Link>
-          <Link to="/cart">Cart</Link>
-          <Link to="/profile">Profile</Link>
-          {isAdmin ? (
-            <Link to="/admin-portal" className="account-links">
-              Admin Portal
-            </Link>
-          ) : null}
-          <a className="account-links" href="#" onClick={handleClick}>
-            Logout
-          </a>
+  <nav>
+    {isLoggedIn ? (
+      <div>
+        {/* The navbar will show these links after you log in */}
+        <Link to="/">Home</Link>
+        <Link to="/products">All Products</Link>
+        <Link to="/cart">Cart</Link>
+        <Link to="/profile">Profile</Link>
+        <header>Orange Ostriches Space Co.</header>
+        {isAdmin ? (
+          <Link to="/admin-portal" className="account-links">
+            Admin Portal
+          </Link>
+        ) : null}
+        <a className="account-links" href="#" onClick={handleClick}>
+          Logout
+        </a>
+      </div>
+    ) : (
+      <div>
+        {/* The navbar will show these links before you log in */}
+        <Link to="/">Home</Link>
+        <Link to="/products">All Products</Link>
+        <header>Orange Ostriches Space Co.</header>
+        <Link to="/cart">Cart</Link>
+        <div className="account-links">
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
         </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/">Home</Link>
-          <Link to="/products">All Products</Link>
-          <Link to="/cart">Cart</Link>
-          <div className="account-links">
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+      </div>
+    )}
+  </nav>
 );
 
 /**
